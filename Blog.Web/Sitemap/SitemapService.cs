3bd20320bae969ffbf775413034865f20682424b
Sebastien OLLIVIER <sebastien.ollivier@gmail.com>
2017-10-03 22:32:12 +0200

    #484 Add sitemap service

---
+++ Blog.Web/Sitemap/SitemapService.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Domain;
+using Blog.Domain.Queries;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace Blog.Web.Sitemap
+{
+    public class SitemapService : SitemapGenerator
+    {
+        private readonly IDistributedCache distributedCache;
+        private readonly ILogger<SitemapService> logger;
+        private readonly IUrlHelper urlHelper;
+        private readonly TimeSpan expirationDuration;
+        private QueryCommandBuilder queryCommandBuilder;
+
+        public SitemapService(IDistributedCache distributedCache, ILogger<SitemapService> logger, IUrlHelperFactory urlHelperFactory, IActionContextAccessor actionContextAccessor, QueryCommandBuilder queryCommandBuilder)
+        {
+            CacheProfile cacheProfile = new CacheProfile { Duration = 86400 };
+            this.expirationDuration = TimeSpan.FromSeconds(cacheProfile.Duration.Value);
+            this.distributedCache = distributedCache;
+            this.logger = logger;
+            this.urlHelper = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext);
+            this.queryCommandBuilder = queryCommandBuilder;
+        }
+
+        public async Task<string> GetSitemapXml(int? index = null)
+        {
+            var sitemapDocuments = await this.distributedCache.GetAsJsonAsync<List<string>>("Sitemap");
+            if (sitemapDocuments == null)
+            {
+                IReadOnlyCollection<SitemapNode> sitemapNodes = GetSitemapNodes();
+                sitemapDocuments = GetSitemapDocuments(sitemapNodes);
+                await this.distributedCache.SetAsJsonAsync("Sitemap", sitemapDocuments,
+                    options: new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = this.expirationDuration
+                    });
+            }
+
+            if (index.HasValue && ((index < 1) || (index.Value >= sitemapDocuments.Count())))
+            {
+                return null;
+            }
+
+            return sitemapDocuments[index ?? 0];
+        }
+
+        protected virtual IReadOnlyCollection<SitemapNode> GetSitemapNodes()
+        {
+            DateTime now = DateTime.Now;
+
+            // Fixed pages -> Home & Home blog
+            var nodes = new List<SitemapNode>
+            {
+                new SitemapNode(this.urlHelper.Action("Index", "Home", null, "https"))
+                {
+                    Priority = 1,
+                    LastModified = now,
+                    Frequency = SitemapFrequency.Always
+                },
+                new SitemapNode(this.urlHelper.Action("List", "Blog", null, "https"))
+                {
+                    Priority = 1,
+                    LastModified = now,
+                    Frequency = SitemapFrequency.Always
+                }
+            };
+
+            // Categories pages
+            var categories = this.queryCommandBuilder.Build<GetCategoriesQuery>().Build().Select(c => c.Code);
+            foreach (var categoryCode in categories)
+            {
+                nodes.Add(new SitemapNode(this.urlHelper.Action("List", "Blog", new { categoryCode = categoryCode }, "https"))
+                {
+                    Priority = 0.5,
+                    LastModified = now,
+                    Frequency = SitemapFrequency.Always
+                });
+            }
+
+            // Posts pages
+            var posts = this.queryCommandBuilder.Build<GetPostsQuery>().Build().Select(p => new { Url = p.Url, CategoryCode = p.Category.Code });
+            foreach (var post in posts)
+            {
+                nodes.Add(new SitemapNode(this.urlHelper.Action("Index", "Post", new { categoryCode = post.CategoryCode, postUrl = post.Url }, "https"))
+                {
+                    Priority = 0.8,
+                    LastModified = now,
+                    Frequency = SitemapFrequency.Always
+                });
+            }
+
+            return nodes;
+        }
+
+        protected override string GetSitemapUrl(int index)
+        {
+            return this.urlHelper.Action("SitemapXml", "Seo", null, "https").TrimEnd('/') + "?index=" + index;
+        }
+
+        protected override void LogWarning(Exception exception)
+        {
+            this.logger.LogWarning(exception.Message, exception);
+        }
+    }
+}
