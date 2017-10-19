6fe68a8345a9a23dc94e5ca0dd32fc8231a8861a
Sebastien OLLIVIER <sebastien.ollivier@gmail.com>
2017-10-19 15:15:22 +0200

    Add tags to meta node

--- Blog.Data/BlogContext.cs
+++ Blog.Data/BlogContext.cs
@@ -15,8 +15,10 @@ namespace Blog.Data
             {
                 post.HasKey(p => p.Id);
                 post.ToTable("Posts");
+                post.Property(p => p.InternalTags).HasColumnName("Tags");
 
                 post.Ignore(p => p.ComputedDescription);
+                post.Ignore(p => p.Tags);
             });
 
             modelBuilder.Entity<Category>(category =>
