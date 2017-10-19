ac0e04005f4b9fc59953a7d9af0e3bc4772afdaf
Sebastien OLLIVIER <sebastien.ollivier@gmail.com>
2017-10-19 18:48:17 +0200

    Refacto and simplify sitemap generation

---
+++ Blog.Web/Sitemap/SitemapBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Blog.Web.Sitemap
+{
+    public class SitemapBuilder
+    {
+        private readonly XNamespace NS = "http://www.sitemaps.org/schemas/sitemap/0.9";
+        private readonly List<SitemapNode> nodes = new List<SitemapNode>();
+
+        public void AddUrl(string url, DateTime? modified = null, ChangeFrequency? changeFrequency = null, double? priority = null)
+        {
+            this.AddUrl(new SitemapNode
+            {
+                Url = url,
+                Modified = modified,
+                ChangeFrequency = changeFrequency,
+                Priority = priority,
+            });
+        }
+
+        public void AddUrl(SitemapNode node)
+        {
+            this.nodes.Add(node);
+        }
+
+        public override string ToString()
+        {
+            var sitemap = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement(NS + "urlset", this.nodes.Select(CreateItemElement)));
+
+            return sitemap.ToString();
+        }
+
+        private XElement CreateItemElement(SitemapNode url)
+        {
+            var itemElement = new XElement(NS + "url", new XElement(NS + "loc", url.Url.ToLower()));
+
+            if (url.Modified.HasValue)
+            {
+                itemElement.Add(new XElement(NS + "lastmod", url.Modified.Value.ToString("yyyy-MM-ddTHH:mm:ss.f") + "+00:00"));
+            }
+
+            if (url.ChangeFrequency.HasValue)
+            {
+                itemElement.Add(new XElement(NS + "changefreq", url.ChangeFrequency.Value.ToString().ToLower()));
+            }
+
+            if (url.Priority.HasValue)
+            {
+                itemElement.Add(new XElement(NS + "priority", url.Priority.Value.ToString("N1")));
+            }
+
+            return itemElement;
+        }
+    }
+}
--- Blog.Web/Sitemap/SitemapNode.cs
+++ Blog.Web/Sitemap/SitemapNode.cs
@@ -3,18 +3,10 @@ using System;
 
 namespace Blog.Web.Sitemap
 {
-    public sealed class SitemapNode
+    public class SitemapNode
     {
-        public SitemapNode(string url)
-        {
-            this.Url = url;
-            this.Priority = null;
-            this.Frequency = null;
-            this.LastModified = null;
-        }
-
-        public SitemapFrequency? Frequency { get; set; }
-        public DateTime? LastModified { get; set; }
+        public ChangeFrequency? ChangeFrequency { get; set; }
+        public DateTime? Modified { get; set; }
         public double? Priority { get; set; }
         public string Url { get; set; }
     }
