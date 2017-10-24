f1064b0dcf9f6a7246999e05060dd07c858f393f
Sebastien OLLIVIER <sebastien.ollivier@gmail.com>
2017-10-24 10:07:51 +0200

    Add IsPublished expression and iqueryableextension Update query to use iqueryableextension

---
+++ Blog.Domain/Entities/PostExtension.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Data;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Blog.Domain.Entities
+{
+    public static class PostExtension
+    {
+        public static Expression<Func<Post, bool>> IsPublishedExpression = post => post.PublicationDate.HasValue && post.PublicationDate < DateTime.Now;
+
+        public static IQueryable<Post> PublishedOnly(this IQueryable<Post> queryable)
+        {
+            return queryable.Where(IsPublishedExpression);
+        }
+    }
+}
