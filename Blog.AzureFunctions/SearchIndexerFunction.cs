c95ca790407991f95aac2c9f94000361ff828ef7
Sebastien OLLIVIER <sebastien.ollivier@gmail.com>
2017-11-08 10:53:29 +0100

    Change timer trigger to run at 2:00 AM every day

--- Blog.AzureFunctions/SearchIndexerFunction.cs
+++ Blog.AzureFunctions/SearchIndexerFunction.cs
@@ -1,11 +1,7 @@
-using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using System.Configuration;
 using System.Threading.Tasks;
-using Blog.Data;
-using Blog.Domain.Queries;
-using Microsoft.EntityFrameworkCore;
 using Blog.Functions.SearchIndexer.BindingRedirectHelper;
 using Blog.SearchIndexer;
 
@@ -19,10 +15,8 @@ namespace Blog.Functions.SearchIndexer
         }
 
         [FunctionName("SearchIndexerFunction")]
-        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static async Task Run([TimerTrigger("0 0 2 * * *")]TimerInfo myTimer, TraceWriter log)
         {
-            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
-
             var searchIndexer = new Indexer(ConfigurationManager.AppSettings["BlogConnection"], ConfigurationManager.AppSettings["AzureSearchName"], ConfigurationManager.AppSettings["AzureSearchKey"], ConfigurationManager.AppSettings["AzureSearchIndexName"]);
             await searchIndexer.LaunchIndexation();
         }
