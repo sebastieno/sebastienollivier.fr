1094f07987f6cabbdfa083d185634f36e4164a01
William KLEIN <wklein@infinitesquare.com>
2018-01-15 22:48:36 +0100

    transition conservant le scroll

---
+++ Blog.Web/ClientApp/app/components/scroll-container/scroll-container.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-container',
+  templateUrl: './scroll-container.component.html',
+  styleUrls: ['./scroll-container.component.scss']
+})
+export class ScrollContainerComponent { }
