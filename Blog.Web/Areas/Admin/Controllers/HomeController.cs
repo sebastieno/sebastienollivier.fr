f3bd257a1f5276419e8b6cb4c26db349d6a8700e
Sébastien <sebastien.ollivier@gmail.com>
2018-03-01 11:32:18 +0100

    Add BO Area + Markdown editor

---
+++ Blog.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Blog.Domain;
+using Blog.Domain.Queries;
+using Blog.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("admin")]
+    public class HomeController : Controller
+    {
+        private readonly QueryCommandBuilder queryCommandBuilder;
+
+        public HomeController(QueryCommandBuilder queryCommandBuilder)
+        {
+            this.queryCommandBuilder = queryCommandBuilder;
+        }
+
+        [Route("")]
+        public async Task<IActionResult> List()
+        {
+            var query = this.queryCommandBuilder.Build<GetPostsQuery>().WithDrafts().Build();
+
+            return View((await query.ToListAsync()).Select(PostModel.FromPost));
+        }
+
+        [Route("{categoryCode}/{postUrl}")]
+        public async Task<IActionResult> Edit(string categoryCode, string postUrl)
+        {
+            var post = await this.queryCommandBuilder.Build<GetPostQuery>().ExecuteAsync(categoryCode, postUrl);
+            if (post == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return View(PostModel.FromPost(post));
+        }
+    }
+}
